02f878fe7cce8f85ccee60fb098b6fa2ea800299
Kyle Mitchell <kyle@kemitchell.com>
2015-04-15 02:41:19 -0700

    prefix cross-references with "Section "

--- source/templates/run.js
+++ source/templates/run.js
@@ -73,7 +73,7 @@ module.exports = function run(element, numberStyle, conspicuous) {
     ) {
       text = BLANK;
     } else {
-      text = numberStyle(element.reference);
+      text = 'Section ' + numberStyle(element.reference);
       properties = {underline: true};
     }
   } else {
