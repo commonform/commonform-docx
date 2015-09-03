4aff289aacdd27075886e96248c532105af26e78
Kyle Mitchell <kyle@kemitchell.com>
2015-09-03 16:04:59 -0700

    Remove hard-coded "Section" prefix

--- templates/run.js
+++ templates/run.js
@@ -57,7 +57,7 @@ module.exports = function run(element, numberStyle, conspicuous) {
     { text = BLANK }
     else {
       text = (
-        'Section ' + numberStyle(numbering) +
+        numberStyle(numbering) +
         ' (' + heading + ')' )
       properties.underline = true } }
   else {
