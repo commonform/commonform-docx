24de69b022cf08cb1b73681e2fda65cdaa4b3ebf
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-15 17:19:44 -0700

    Render <w:sectPr> to set page size and margins

--- templates/document.js
+++ templates/document.js
@@ -26,6 +26,15 @@ var DOCUMENT_XMLNS = (
 /* jscs:enable maximumlinelength */
 )
 
+var SECTION = (
+'<w:sectPr>' +
+'  <w:pgSz w:w="12240" w:h="15840"/>' +
+'  <w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="720" w:footer="720" w:gutter="0"/>' +
+'  <w:cols w:space="720"/>' +
+'  <w:docGrid w:linePitch="360"/>' +
+'</w:sectPr>'
+)
+
 module.exports = function (
   form, values, title, edition, hash,
   centerTitle, numberStyle, indentMargins, after, blanks, markFilled
@@ -45,6 +54,7 @@ module.exports = function (
         (hash ? hashRun(hash, centerTitle) : '') +
         paragraphs +
         after +
+        SECTION +
       '</w:body>' +
     '</w:document>'
   )
