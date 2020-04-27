136993e2fd4a6c66d480b43e2bdf1854a1605a82
Kyle E. Mitchell <kyle@kemitchell.com>
2020-04-26 23:49:01 -0700

    Support components

--- templates/paragraph.js
+++ templates/paragraph.js
@@ -67,13 +67,62 @@ module.exports = function (
         )
         : ''
     ) +
-    element.content
-      .map(function (element) {
-        return makeRun(element, conspicuous)
-      })
-      .join('')
+    (
+      has(element, 'repository') // component
+        ? componentToContent(element)
+          .map(function (element) {
+            return makeRun(element, conspicuous)
+          })
+          .join('')
+        : element.content
+          .map(function (element) {
+            return makeRun(element, conspicuous)
+          })
+          .join('')
+    )
   )
   function makeRun (element, conspicuous) {
     return run(element, numberStyle, conspicuous, blanks, markFilled, styles)
   }
+}
+
+function componentToContent (component) {
+  var url = 'https://' + [
+    component.repository,
+    component.publisher,
+    component.project,
+    component.edition
+  ].map(encodeURIComponent).join('/')
+  var returned = []
+  var substitutions = component.substitutions
+  var hasSubstitutions = (
+    Object.keys(substitutions.terms).length > 0 ||
+    Object.keys(substitutions.headings).length > 0
+  )
+  var firstString = url
+  if (hasSubstitutions) {
+    if (component.upgrade) firstString += ' with updates and corrections, replacing '
+    else firstString += ' replacing '
+    returned.push(firstString)
+    var substitutionContent = []
+    Object.keys(substitutions.terms).forEach(function (from) {
+      var to = substitutions.terms[from]
+      substitutionContent.push(from + ' with ', { use: to })
+    })
+    Object.keys(substitutions.headings).forEach(function (from) {
+      var to = substitutions.headings[from]
+      substitutionContent.push(from + ' with ', { referene: to })
+    })
+    var length = substitutionContent.length
+    for (var index = 2; index < length - 1; index += 2) {
+      substitutionContent[index] = ', ' + substitutionContent[index]
+    }
+    substitutionContent.forEach(function (element) {
+      returned.push(element)
+    })
+  } else {
+    if (component.upgrade) firstString += ' with updates and corrections'
+    returned.push(firstString)
+  }
+  return returned
 }
